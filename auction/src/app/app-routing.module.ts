07acb9d00fbbf7c290913d3ee21d54f0a41f6494
cole-yu <1032577646@qq.com>
2018-05-14 23:46:56 +0800

    修复结构调整后的路由bug

--- auction/src/app/app-routing.module.ts
+++ auction/src/app/app-routing.module.ts
@@ -2,21 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import {Routes,RouterModule} from '@angular/router';
 import {HomeComponent} from './home/home.component';
-import {ProductComponent} from './product/product.component';
 import {UnitComponent} from './unit/unit.component';
 import {ProductDescComponent} from './product-desc/product-desc.component';
 import {SellerInfoComponent} from './seller-info/seller-info.component';
 import {ChatComponent} from './chat/chat.component';
 import {AuthGuard} from './guard/auth.guard';
 import {UnsavedGuard} from './guard/unsaved.guard';
-import  {LoginComponent} from './login/login.component';
+import {LoginComponent} from './login/login.component';
 import {FirstpageComponent} from './firstpage/firstpage.component';
 
 const  routes: Routes = [
     { path:'',redirectTo:'login',pathMatch:'full'},
     { path: 'login' , component:LoginComponent},
-    { path:"user",component:FirstpageComponent,children:[
+    { path:'user',component:FirstpageComponent,children:[
         { path: '' , component:HomeComponent},
+        { path: 'home' , component:HomeComponent},
         { path:'unit/:id',component:UnitComponent,children:[
             {path:'',component:ProductDescComponent},
             {path:'seller/:id',component:SellerInfoComponent},
